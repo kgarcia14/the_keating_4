5a2ca343384a8615b2fc7e578169e7b636e553f1
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2020-12-19 09:12:06 -0500

    created parameters that state whether an item helps the body of the character. Implemented those features in the class method. Added functionality to 'on pick up' feature within the item picked up method

--- classes.py
+++ classes.py
@@ -1,9 +1,13 @@
 
 class Character:
-    def __init__(self, name, alert_level):
+    def __init__(self, name, alert_level, ability):
         self.name = name
         self.alert_level = alert_level
         self.items = []
+        self.ability = ability
+        self.hands = ["hands", True]
+        self.shirt = ["shirt", True]
+        self.parts = [self.hands, self.shirt]
 
     def change_alert(self, location, item_idx):
         self.alert_level += location.item[item_idx]
@@ -13,18 +17,32 @@ class Character:
 
     # item limit within list: cap number of items and allow user to get rid of an item
     def add_items(self, item):
-        self.items.append(item)
+        if item.on_use == False:
+            for part in self.parts:
+                if item.character_part == part[0]:
+                    part[1] = False
+        else:
+            self.items.append(item)
+        self.alert_level += item.alert_effect_pu
+
 
 # Create method for using items(when items are used they affect alert level => items.alert_effect_used) Kurtis
+
     def item_used(self, item):
         self.alert_level += item.alert_effect_used
+        if item.on_use == True:
+            for part in self.parts:
+                if item.character_part == part[0]:
+                    part[1] = False
         self.items.remove(item)
 
         # Create method for removing items(when the character wants to remove the items they get removed from the list)JoJo
-    def remove_items(self, item):
-        self.item.pop
+    # def remove_items(self, item):
+    #     self.item.pop
 
-
+    def __str__(self):
+        blurb = f"You have chosen {self.name}!\nCongrats! {self.name} has a starting alert level of {self.alert_level}.\n Also, surprise! Each character has a hidden special ability. We like to {self.name}'s: {self.ability}"
+        return
 
 
 class Location:
@@ -34,8 +52,10 @@ class Location:
 
 
 class Item:
-    def __init__(self, name, associated_room, alert_effect_pu=0, alert_effect_used=0):
+    def __init__(self, name, associated_room, character_part="body", on_use=True, alert_effect_pu=0, alert_effect_used=0):
         self.name = name
         self.associated_room = associated_room
+        self.character_part = character_part
+        self.on_use = on_use
         self.alert_effect_pu = alert_effect_pu
         self.alert_effect_used = alert_effect_used
