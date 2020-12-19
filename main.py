ac022ddef1eab7bc6ebef8ad4796371fef7aa37f
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2020-12-18 23:16:07 -0500

    Fixed bugs in menu items, got rid of unused variable: elevator only, moved functions for menu items outside of game play.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ annalise = Character('Annalise Keating', 0)
 main_players = [crystal, jojo, kurtis, joshua]
 
 # Location Instantiation
-josh_room = Location("Quiet Room", "pass")
+josh_room = Location("Sean's hide-out", "pass")
 liz_office = Location("Liz's Office", "pass")
 elevator = Location("Elevator", "pass")
 roof = Location("Roof", "pass")
@@ -22,12 +22,15 @@ security_desk = Location("Security Desk", "pass")
 parking_garage = Location("Parking Garage", "pass")
 community_center = Location("Community Center", "pass")
 location_keys = {
-    josh_room: [liz_office, kitchen],
-    liz_office: [josh_room, kitchen],
-    kitchen: [liz_office, josh_room],
-    elevator: [liz_office, josh_room, kitchen, roof, community_center, gym]
+    josh_room: [liz_office, kitchen, elevator],
+    liz_office: [josh_room, kitchen, elevator],
+    kitchen: [liz_office, josh_room, elevator],
+    elevator: [liz_office, josh_room, kitchen, roof, community_center, gym],
+    roof: [elevator],
+    community_center: [elevator],
+    gym: [elevator]
 }
-elevator_only_locations = [roof, community_center, gym]
+
 
 
 # Items Instantiation
@@ -80,8 +83,8 @@ pillows = Item("Pillow", community_center)
 ping_pong = Item("Ping Pong", community_center, -10)
 rug = Item("Rug", community_center, -10)
 blankets = Item("Blanket", community_center, -15)
-items_community_center = [arcade_game.name,
-                          pillows.name, ping_pong.name, rug.name, blankets.name]
+items_community_center = [arcade_game,
+                          pillows, ping_pong, rug, blankets]
 
 all_items = [monitor, jacket, bag_of_chips, tshirt, book, keyboard, key_card, trash_can, tarp, chair, firepit, sink, bleach, freezer, shower, shower_curtain, weights, security_desk, get_away_car, arcade_game, pillows, ping_pong, rug, blankets]
 
@@ -104,6 +107,7 @@ def print_string_menu(menu):
 
 def print_menu(menu):
     for idx, choice in enumerate(menu):
+        # print(type(choice))
         print(f"{idx+1}. {choice.name}")
 
 def print_location_options(location_options):
@@ -111,7 +115,7 @@ def print_location_options(location_options):
     for location in location_options:
         location_name_list.append(location)
     for idx, choice in enumerate(location_name_list):
-        print(f"{idx+1}. {choice}")
+        print(f"{idx+1}. {choice.name}")
 
 def which_list(room):
     if room == josh_room:
@@ -119,18 +123,21 @@ def which_list(room):
     if room == gym:
         return items_gym
 
-def main_menu_choice_1(list, player):
+def remove_item_from_inventory(player):
+    print_menu(player.items)
+    full_menu_remove_choice = int(input("Which item would you like to remove?"))
+    player.items.pop(full_menu_remove_choice - 1)
+    print("Your item has been removed")
+
+def main_menu_choice_1(list_option, player):
     item_loop = True
     while item_loop:
-        print_menu(list)
+        print_menu(list_option)
         if len(player.items) >= 5:
             full_menu_choice = (input(
                 "You are already holding the maximum number of items. Would you like to remove one? y or n:  "))
             if full_menu_choice.lower() == "y":
-                print_menu(player.items)
-                full_menu_remove_choice = int(
-                    input("Which item would you like to remove?"))
-                player.items.pop(full_menu_remove_choice - 1)
+                remove_item_from_inventory(active_player)
             elif full_menu_choice == "n":
                 pass
             # incorrect responce error catch
@@ -143,8 +150,8 @@ def main_menu_choice_1(list, player):
                 if item_chosen == 9:
                     item_loop = False
                 else:
-                    player.add_items(list[item_chosen-1])
-                    list.pop(item_chosen-1)
+                    player.add_items(list_option[item_chosen-1])
+                    list_option.pop(item_chosen-1)
                     continue_choosing = input(
                     "Would you like to choose another item? y or n\n")
                     if continue_choosing.lower() == "n":
@@ -152,11 +159,37 @@ def main_menu_choice_1(list, player):
             except ValueError:
                 print("Please choose an available menu choice!")
                 
+def main_menu_choice_2(location, player, list_option):
+    print_menu(player.items)
+    if location == josh_room:
+        print_string_menu(list_option)
+        choice_item_use_menu = int(input("Please choose: "))
+        if choice_item_use_menu == 1:
+            print_menu(player.items)
+            user_choice = int(input("Choose which item to use:"))
+            item_being_used = player.items[user_choice-1]
+            player.item_used(item_being_used)
+        elif choice_item_use_menu == 2:  # get rid of item: print items and ask to choose an item to get rid of. run method for removing item JoJo
+            remove_item_from_inventory(player)
 
+def main_menu_choice_3(location):
+    location_list = []
+    if len(location_keys[location]) == 1 and location_keys[location][0] == elevator:
+        new_location = elevator
+        print("\n\n***You must take the elevator to your next location.***")
+    else:
+        for location in location_keys[location]:
+            location_list.append(location)
+        print("Where would you like to go?")
+        print_location_options(location_list)
+        location_choice = int(input("Please choose: "))
+        new_location = location_list[location_choice - 1]
+    print(f"\n\n***You are now in the {new_location.name}***")
+    return new_location
 
 def play_game():
 
-    location_choice_list = [josh_room.name, elevator.name]
+    # location_choice_list = [josh_room.name, elevator.name]
     print("Welcome to How To Get Away With Murder")
     # We need some info here about the game....so you know what character you want to choose, must use items in Josh's room!
     print()  # Add in player stats and special characteristics
@@ -186,37 +219,10 @@ def play_game():
         main_menu_choice = input("Please choose: ")
         if main_menu_choice == "1":
             main_menu_choice_1(curr_items_list, active_player)
-
-
         if main_menu_choice == "2":
-            print_menu(active_player.items)
-            if curr_location == josh_room:
-                print_string_menu(item_use_menu)
-                choice_item_use_menu = int(input("Please choose: "))
-                if choice_item_use_menu == 1:
-                    print_menu(active_player.items)
-                    user_choice = int(input("Choose which item to use:"))
-                    item_being_used = active_player.items[user_choice-1]
-                    active_player.item_used(item_being_used)
-                    
-
-                elif choice_item_use_menu == 2:  # get rid of item: print items and ask to choose an item to get rid of. run method for removing item JoJo
-                    print("Which item would you like get rid of?")
-                    print_menu(active_player.items)
-
+            main_menu_choice_2(curr_location, active_player, item_use_menu)
         if main_menu_choice == "3":  # Move to a new location
-            elevator_only = False
-            for location in elevator_only_locations:
-                if location == curr_location:
-                    elevator_only = True
-            if elevator_only == False:
-                for location in location_keys[curr_location]:
-                    location_choice_list.append(location)
-            print(
-                f"Where would you like to go?\n{print_location_options(location_choice_list)}")
-            location_choice = int(input("Please choose: "))
-            curr_location = location_choice_list[location_choice - 1]
-            print(curr_location)
+            curr_location = main_menu_choice_3(curr_location)
         if main_menu_choice == "4":
             play_game = False
 
